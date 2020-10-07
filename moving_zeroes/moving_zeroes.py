2845b1ffe5681c6e76be74ea6854a21f47cc5873
Sean Birmingham <seannbirmingham98@gmail.com>
2020-10-07 18:27:56 -0400

    finished moving zeros and product algorithms

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,22 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+    # counting all zeros
+    zeros = arr.count(0)
+    next_index = 0
+
+    for n in arr:
+        if n != 0:
+            arr[next_index] = n
+            next_index += 1
+
+    for zero in range(1, zeros + 1):
+        arr[-zero] = 0
+
+    return arr
 
 
 if __name__ == '__main__':
--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,15 +2,31 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def product_of_all_other_numbers(arr):
-    # Your code here
 
-    pass
+
+def product_of_all_other_numbers(arr):
+    n = len(arr)
+
+    left = [None] * n
+    right = [None] * n
+
+    left[0] = 1
+    for i in range(1, n):
+        left[i] = arr[i - 1] * left[i - 1]
+    right[n - 1] = 1
+    for j in reversed(range(n - 1)):
+        right[j] = arr[j + 1] * right[j + 1]
+    for i in range(n):
+        arr[i] = left[i] * right[i]
+
+    return arr
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8,
+    #        9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    print(
+        f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
